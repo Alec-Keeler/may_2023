764afb7e8b16a4fa61c192c4030442a3b62a06d0
Alec Keeler <alec.keeler@gmail.com>
2023-07-18 10:48:16 -0500

    student practices

--- demo_code/app.js
+++ demo_code/app.js
@@ -3,14 +3,36 @@ const app = express()
 
 app.use(express.json())
 
-app.use('/', (req, res, next) => {
+app.use('/test', (req, res, next) => {
 	console.log(`path: ${req.path}`)
 	next()
 })
 
-app.get('/test', (request, response) => {
-	response.send('Hello from your first express API')
+app.use((req, res, next) => {
+	console.log(`path: ${req.path}`)
+	next()
 })
+
+app.use((req, res, next) => {
+	console.log('error test')
+	const error = 'There was an error :('
+	next()
+})
+
+const test = (req, res, next) => {
+	res.send('Hello from your first express API')
+	// next()
+}
+
+const checkUserInput = (req, res, next) => {
+	if (!req.body.stuff) {
+		return res.send('please include a stuff property')
+	}
+	console.log('check user input')
+	next()
+}
+
+app.get('/test', checkUserInput, test)
 
 app.post('/create', (req, res) => {
 	console.log(req.body)
@@ -33,6 +55,11 @@ app.get('/actor/:actorId/movies/:movieId', (req, res) => {
 app.get('/search', (req, res) => {
 	console.log(req.query)
 	res.send('search filters')
+})
+
+app.use((err, req, res, next) => {
+	console.log(err)
+	res.send(err)
 })
 
 app.listen(8000, () => console.log('Listening on port 8000...'))
